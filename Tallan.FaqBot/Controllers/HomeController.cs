ba36c95b662bcce0c942c6475e8ca0b6741a586c
Ryan O'Flaherty <ryan.oflaherty@tallan.com>
2017-07-06 08:12:12 -0400

    add company name to website

--- Tallan.FaqBot/Controllers/HomeController.cs
+++ Tallan.FaqBot/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
 
 namespace Tallan.FaqBot.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string clientCompanyKey = "clientCompany";
+
         /// <summary>
         /// Homepage for the FAQ bot demo.
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
         {
+            var appSettings = ConfigurationManager.AppSettings;
+            string result = appSettings[clientCompanyKey] ?? "Company Not Found";
+            ViewData["ClientCompany"] = result;
             return View();
         }
     }
